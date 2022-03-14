e57203790ce15a02b0fb969071f736881ad567e8
AnjaMis <bgp916@myy.haaga-helia.fi>
2022-03-14 23:11:08 +0200

    added firebase connection

--- screens/Login.js
+++ screens/Login.js
@@ -1,4 +1,4 @@
-import React, {useContext, useState} from 'react';
+import React, { useContext, useState } from 'react'
 import {
   View,
   Text,
@@ -8,24 +8,55 @@ import {
   StyleSheet,
   ScrollView,
   Button,
-  TextInput
-} from 'react-native';
-import {windowHeight, windowWidth} from '../utils/Dimensions';
-import FormInput from '../components/FormInput';
-import FormButton from '../components/FormButton';
-import SocialButton from '../components/SocialButton';
+  TextInput,
+} from 'react-native'
+import { windowHeight, windowWidth } from '../utils/Dimensions'
+import FormInput from '../components/FormInput'
+import FormButton from '../components/FormButton'
+import SocialButton from '../components/SocialButton'
 
+// importing firebase features
+import { initializeApp } from 'firebase/app'
+import { getDatabase, push, ref, onValue } from 'firebase/database'
+import { getAnalytics } from 'firebase/analytics'
 
-const Login = ({navigation}) => {
-  const [email, setEmail] = useState();
-  const [password, setPassword] = useState();
+// web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyDcdYiAl0P6_LZNxoxTgD1fPTyE6EpEfsg',
+  authDomain: 'bonapp-7cc65.firebaseapp.com',
+  databaseURL:
+    'https://bonapp-7cc65-default-rtdb.europe-west1.firebasedatabase.app',
+  projectId: 'bonapp-7cc65',
+  storageBucket: 'bonapp-7cc65.appspot.com',
+  messagingSenderId: '198916352344',
+  appId: '1:198916352344:web:c08718687f9d0af4df9d2d',
+  measurementId: 'G-G6MJH49PMZ',
+}
+
+// initializing firebase
+const app = initializeApp(firebaseConfig)
+const database = getDatabase(app)
+const analytics = getAnalytics(app)
+
+const Login = ({ navigation }) => {
+  // defining the collection name
+  ref(database, 'Client/')
+
+  const [email, setEmail] = useState()
+  const [password, setPassword] = useState()
+
+  // saving log in details to the firebase
+  const saveLogin = () => {
+    push(ref(database, 'Client/'), { email: email, password: password })
+    navigation.navigate('Home')
+  }
 
   return (
     <View style={styles.container}>
       <Image source={require('../assets/logo.png')} style={styles.logo} />
       <FormInput
         labelValue={email}
-        onChangeText={userEmail => setEmail(userEmail)}
+        onChangeText={(userEmail) => setEmail(userEmail)}
         placeholderText="Email"
         iconType="user"
         keyboardType="email-address"
@@ -35,16 +66,13 @@ const Login = ({navigation}) => {
 
       <FormInput
         labelValue={password}
-        onChangeText={userPassword => setPassword(userPassword)}
+        onChangeText={(userPassword) => setPassword(userPassword)}
         placeholderText="Password"
         iconType="lock"
         secureTextEntry={true}
       />
 
-      <FormButton
-        buttonTitle="Sign In"
-        onPress={() => navigation.navigate("Home")}
-      />
+      <FormButton buttonTitle="Sign In" onPress={saveLogin} />
 
       <TouchableOpacity style={styles.forgotButton} onPress={() => {}}>
         <Text style={styles.navButtonText}>Forgot Password?</Text>
@@ -52,38 +80,33 @@ const Login = ({navigation}) => {
 
       <TouchableOpacity
         style={styles.forgotButton}
-        onPress={() => navigation.navigate('SignUp')}>
+        onPress={() => navigation.navigate('SignUp')}
+      >
         <Text style={styles.navButtonText}>
           Don't have an acount? Create here
         </Text>
       </TouchableOpacity>
 
-      
-        
-          <SocialButton
-            buttonTitle="Sign In with Facebook"
-            btnType="facebook"
-            color="#4867aa"
-            backgroundColor="#e6eaf4"
-            onPress={() => fbLogin()}
-          />
+      <SocialButton
+        buttonTitle="Sign In with Facebook"
+        btnType="facebook"
+        color="#4867aa"
+        backgroundColor="#e6eaf4"
+        onPress={() => fbLogin()}
+      />
 
-          <SocialButton
-          
-            buttonTitle="Sign In with Google"
-            btnType="google"
-            color="#de4d41"
-            backgroundColor="#f5e7ea"
-            onPress={() => googleLogin()}
-          />
-    
-
-      
+      <SocialButton
+        buttonTitle="Sign In with Google"
+        btnType="google"
+        color="#de4d41"
+        backgroundColor="#f5e7ea"
+        onPress={() => googleLogin()}
+      />
     </View>
-  );
-};
+  )
+}
 
-export default Login;
+export default Login
 
 const styles = StyleSheet.create({
   container: {
@@ -118,4 +141,4 @@ const styles = StyleSheet.create({
     color: '#4BBE77',
     marginTop: 5,
   },
-});
+})
