07e0d72c763e6ddb15304fb2a28164b4a4f5be23
Izmoreno <ismael.moreno@students.hevs.ch>
2022-04-02 13:47:25 +0200

    add Drawer Menu with new Navigation

--- App.js
+++ App.js
@@ -2,66 +2,122 @@ import "react-native-gesture-handler";
 import React, { useEffect, useState } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import { createDrawerNavigator } from '@react-navigation/drawer';
 import { LoginScreen, HomeScreen, RegistrationScreen } from "./screens";
 import RestaurantSignup from "./screens/RegistrationScreen/RestaurantSignup";
 import RestaurantInfo from "./screens/HomeScreen/RestaurantInfo";
 import HomeRestaurantView from "./screens/HomeScreen/HomeRestaurantView";
 import LogoCharging from "./screens/RegistrationScreen/LogoCharging";
-import { LogBox } from 'react-native';import BenefitsScreen from "./screens/HomeScreen/BenefitsScreen";
-;
+import { LogBox } from 'react-native'; import BenefitsScreen from "./screens/HomeScreen/BenefitsScreen";
+
 
 const Stack = createStackNavigator();
-console.ignoredYellowBox = true;
+const Drawer = createDrawerNavigator();
 
+console.ignoredYellowBox = true;
 LogBox.ignoreAllLogs();
+
+const NavigationDrawerStructure = (props) => {
+  //Structure for the navigatin Drawer
+  const toggleDrawer = () => {
+    //Props to open/close the drawer
+    props.navigationProps.toggleDrawer();
+  };
+
+  return (
+    <View style={{ flexDirection: 'row' }}>
+      <TouchableOpacity onPress={toggleDrawer}>
+        {/*Donute Button Image */}
+        <Image
+          source={{
+            uri:
+              'https://raw.githubusercontent.com/AboutReact/sampleresource/master/drawerWhite.png',
+          }}
+          style={{ width: 25, height: 25, marginLeft: 5 }}
+        />
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+function HomeStack({ navigation }) {
+  return (
+    <Stack.Navigator initialRouteName="Home">
+      <Stack.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{ title: "", headerTransparent: true }}
+      />
+      <Stack.Screen
+        name="RestaurantInfo"
+        component={RestaurantInfo}
+        options={{ title: "", headerTransparent: true }}
+      />
+      <Stack.Screen
+        name="HomeRestaurantView"
+        component={HomeRestaurantView}
+        options={{ title: "", headerTransparent: true }}
+      />
+      <Stack.Screen
+        name="LogoCharging"
+        component={LogoCharging}
+        options={{ title: "", headerTransparent: true }}
+      />
+    </Stack.Navigator>
+  );
+}
+
+function AuthStack({ navigation }) {
+  return (
+    <Stack.Navigator initialRouteName="Login">
+      <Stack.Screen
+        name="Login"
+        component={LoginScreen}
+        options={{ title: "", headerTransparent: true }}
+      />
+      <Stack.Screen
+        name="Registration"
+        component={RegistrationScreen}
+        options={{ title: "", headerTransparent: true }}
+      />
+    </Stack.Navigator>
+  );
+}
+
+function JoinUsStack({ navigation }) {
+  return (
+    <Stack.Navigator initialRouteName="JoinUs">
+      <Stack.Screen
+        name="BenefitsScreen"
+        component={BenefitsScreen}
+        options={{ title: "", headerTransparent: true }}
+      />
+      <Stack.Screen
+        name="RestaurantSignUp"
+        component={RestaurantSignup}
+        options={{ title: "", headerTransparent: true }}
+      />
+    </Stack.Navigator>
+  );
+}
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
+      <Drawer.Navigator initialRouteName="Home">
+        <Drawer.Screen
+          name="Home"
+          component={HomeStack}
+        />
+        <Drawer.Screen
           name="Login"
-          component={LoginScreen}
-          options={{ title: "", headerTransparent: true }}
+          component={AuthStack}
         />
-        <Stack.Screen
-          name="Registration"
-          component={RegistrationScreen}
-          options={{ title: "", headerTransparent: true }}
+        <Drawer.Screen
+          name="JoinUs"
+          component={JoinUsStack}
         />
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen
-          name="RestaurantSignup"
-          component={RestaurantSignup}
-          options={{ title: "", headerTransparent: true }}
-        />
-
-        <Stack.Screen
-          name="RestaurantInfo"
-          component={RestaurantInfo}
-          options={{ title: "", headerTransparent: true }}
-        />
-        <Stack.Screen
-          name="HomeRestaurantView"
-          component={HomeRestaurantView}
-          options={{
-            title: "",
-            headerTransparent: true,
-            headerLeft: (props) => null,
-          }}
-        />
-        <Stack.Screen
-          name="LogoCharging"
-          component={LogoCharging}
-          options={{ title: "", headerTransparent: true }}
-        />
-
-        <Stack.Screen
-          name="BenefitsScreen"
-          component={BenefitsScreen}
-          options={{ title: "", headerTransparent: true }}
-        />
-      </Stack.Navigator>
+      </Drawer.Navigator>
     </NavigationContainer>
   );
 }
