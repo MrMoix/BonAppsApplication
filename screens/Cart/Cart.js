57c4a19a3e097f00b1c5ed7d0c77543015307d47
Izmoreno <ismael.moreno@students.hevs.ch>
2022-04-18 00:30:02 +0200

    cart shows Items

--- screens/Cart/Cart.js
+++ screens/Cart/Cart.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import { firebase } from "../../firebase/config";
-import { collection, doc, setDoc, arrayUnion } from "firebase/firestore";
 import {
   Text,
   Image,
@@ -13,31 +12,53 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
 import icons from "../../constants/icons";
 import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
-import { setStatusBarBackgroundColor } from "expo-status-bar";
 
 export default function Cart({ route, navigation }) {
-  const [userData, setUserData] = useState({});
   const [cartItem, setCartItem] = useState([]);
-  const dishList = [];
 
   useEffect(() => {
     async function fetchCart() {
 
       const currentUid = firebase.auth().currentUser.uid;
 
-      const currentUserCart = firebase.firestore().collection('Clients').doc(currentUid).get();
-        
-      dishList.push(currentUserCart.data());
-  }
+      const currentUserCart = await firebase.firestore().collection('Clients').doc(currentUid).get();
+      const items = currentUserCart.data().currentCartItem;
+      let dishList = [];
+
+      items.forEach(element =>  { 
+        dishList.push(element);
+      });
+      setCartItem(dishList);
+    }
 
     fetchCart();
   },);
+  const deleteToCart = (dish) => {
+
+    const uid = firebase.auth().currentUser.uid;
+
+    const userCart = firebase.firestore().collection('Clients').doc(uid);
+    userCart.update({currentCartItem: firebase.firestore.FieldValue.arrayRemove(dish)})
+
+    alert("Item deleted to cart !");
+  }
   const confirmOrder = () => {
     navigation.navigate("TableTimeReservation", {
-      dishList: dishList,
+      dishList: cartItem,
       uid: "2sH1gPONg3MvwYMDn4LU3aGRZAN2",
-      restaurantid: "L81dYiej2fgz6UQFAMV6",
-    })};
+      restaurantid: cartItem.restaurantId,
+    })
+  };
+  const wait = (timeout) => {
+    return new Promise((resolve) => setTimeout(resolve, timeout));
+  };
+
+  const [refreshing, setRefreshing] = React.useState(false);
+
+  const onRefresh = React.useCallback(() => {
+    setRefreshing(true);
+    wait(2000).then(() => setRefreshing(false));
+  }, []);
   return (
     <View
       style={{
@@ -50,12 +71,36 @@ export default function Cart({ route, navigation }) {
       <KeyboardAwareScrollView
         style={{ flex: 1, width: "100%", borderRadius: 5 }}
         keyboardShouldPersistTaps="always"
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        }
       >
         <Image style={styles.logo} source={require("../../assets/logo.png")} />
-        <Text style={styles.restaurantText}> Dish Name : {userData.name}</Text>
+        {cartItem.map((dish) => (
+          <View style={styles.dishBox}>
+            <Image style={styles.logoPicture} source={icons.burger} />
+            <Text style={styles.restaurantText}> Dish Name : {dish.name}</Text>
+            <Text style={styles.restaurantText}>
+              {" "}
+              Dish Price : {dish.price}
+            </Text>
+            <View
+              style={{
+                flex: 1,
+                flexDirection: "row",
+              }}
+            >
+               <TouchableOpacity
+                onPress={() => deleteToCart(dish)}
+              >
+                <Image style={styles.dishBoxIcons} source={icons.delete_icon} />
+              </TouchableOpacity>
+            </View>
+          </View>
+        ))}
         <TouchableOpacity style={styles.button} onPress={() => confirmOrder()}>
-        <Text style={styles.buttonTitle}> Confirm Order </Text>
-      </TouchableOpacity>
+          <Text style={styles.buttonTitle}> Confirm Order </Text>
+        </TouchableOpacity>
       </KeyboardAwareScrollView>
     </View>
   );
