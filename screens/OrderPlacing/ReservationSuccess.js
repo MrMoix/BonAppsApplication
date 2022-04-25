bb8313773b568131eb02b92e7c969a49e192b22c
MrMoix <gaetan.moix@students.hevs.ch>
2022-04-25 10:25:49 +0200

    notifications

--- screens/OrderPlacing/ReservationSuccess.js
+++ screens/OrderPlacing/ReservationSuccess.js
@@ -6,12 +6,41 @@ import {
 } from "react-native";
 import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 import styles from "./styles";
+import * as Notifications from "expo-notifications";
 
 export default function TableTimeReservation({ navigation }) {
 
   const navigateHome = () => {
     navigation.navigate("HomeScreen");
   };
+
+  async function scheduleAndCancel() {
+    Notifications.setNotificationHandler({
+      handleNotification: async () => ({
+        shouldShowAlert: true,
+        shouldPlaySound: true,
+        shouldSetBadge: true,
+        
+      }),
+    });
+    Notifications.scheduleNotificationAsync({
+      content: {
+        title: 'BonApps',
+        body: "Hello,  your order is ready soon !",
+        color: "#4cce4a",
+        subtitle: "BonApps Application",
+        backgroundColor: "#4cce4a",
+        name: 'default',
+        
+      },
+      trigger:{ seconds: 1,}
+    });
+  }
+
+  function combine(){
+    navigateHome();
+    scheduleAndCancel();
+  }
 
   return (
     <View style={styles.container}>
@@ -27,7 +56,7 @@ export default function TableTimeReservation({ navigation }) {
           </Text>
         </View>
         <View>
-          <TouchableOpacity style={styles.button} onPress={navigateHome}>
+          <TouchableOpacity style={styles.button} onPress={() => combine()}>
             <Text style={styles.buttonTitle}>Back to Home</Text>
           </TouchableOpacity>
         </View>
