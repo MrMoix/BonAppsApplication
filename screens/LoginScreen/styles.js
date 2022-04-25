7fcbe90fae50dc6057801b99c0635901e2596c3e
AnjaMis <bgp916@myy.haaga-helia.fi>
2022-04-26 00:57:58 +0300

    logout button fixed

--- screens/LoginScreen/styles.js
+++ screens/LoginScreen/styles.js
@@ -1,69 +1,80 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet } from "react-native";
 
 export default StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        backgroundColor: '#4BBE77'
-    },
-    title: {
-
-    },
-    logo: {
-        flex: 1,
-        height: 150,
-        width: 250,
-        alignSelf: "center",
-        marginBottom: 10
-    },
-    input: {
-        height: 48,
-        borderRadius: 5,
-        overflow: 'hidden',
-        backgroundColor: 'white',
-        marginTop: 10,
-        marginBottom: 10,
-        marginLeft: 30,
-        marginRight: 30,
-        paddingLeft: 16
-    },
-    button: {
-        backgroundColor: '#4cce4a',
-        marginLeft: 30,
-        marginRight: 30,
-        marginTop: 20,
-        height: 48,
-        borderRadius: 5,
-        alignItems: "center",
-        justifyContent: 'center'
-    },
-    googleButton: {
-        backgroundColor: '#24a0ed',
-        marginLeft: 30,
-        marginRight: 30,
-        marginTop: 20,
-        height: 48,
-        borderRadius: 5,
-        alignItems: "center",
-        justifyContent: 'center'
-    },
-    buttonTitle: {
-        color: 'white',
-        fontSize: 16,
-        fontWeight: "bold"
-    },
-    footerView: {
-        flex: 1,
-        alignItems: "center",
-        marginTop: 20
-    },
-    footerText: {
-        fontSize: 16,
-        color: '#2e2e2d'
-    },
-    footerLink: {
-        color: "#ffffff",
-        fontWeight: "bold",
-        fontSize: 16
-    }
-})
+  container: {
+    flex: 1,
+    alignItems: "center",
+    backgroundColor: "#4BBE77",
+  },
+  title: {},
+  logo: {
+    flex: 1,
+    height: 150,
+    width: 250,
+    alignSelf: "center",
+    marginBottom: 10,
+  },
+  logo2: {
+    flex: 1,
+    height: 150,
+    width: 250,
+    alignSelf: "center",
+    marginBottom: 10,
+  },
+  input: {
+    height: 48,
+    borderRadius: 5,
+    overflow: "hidden",
+    backgroundColor: "white",
+    marginTop: 10,
+    marginBottom: 10,
+    marginLeft: 30,
+    marginRight: 30,
+    paddingLeft: 16,
+  },
+  button: {
+    backgroundColor: "#4cce4a",
+    marginLeft: 30,
+    marginRight: 30,
+    marginTop: 20,
+    height: 48,
+    borderRadius: 5,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  googleButton: {
+    backgroundColor: "#24a0ed",
+    marginLeft: 30,
+    marginRight: 30,
+    marginTop: 20,
+    height: 48,
+    borderRadius: 5,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  buttonTitle: {
+    color: "white",
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  NormalText: {
+    color: "white",
+    fontSize: 22,
+    marginTop: 20,
+    textAlign: "center",
+  },
+  footerView: {
+    flex: 1,
+    alignItems: "center",
+    marginTop: 20,
+  },
+  footerText: {
+    fontSize: 16,
+    color: "#2e2e2d",
+  },
+  footerLink: {
+    color: "#ffffff",
+    fontWeight: "bold",
+    fontSize: 16,
+  },
+});
